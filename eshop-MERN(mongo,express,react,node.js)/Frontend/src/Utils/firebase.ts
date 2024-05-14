297f8fd58e3fc88b0784d994d88e4e9ced11a44b
Ehab Abu Hadwan <ehabopto@gmail.com>
2024-05-14 21:55:55 +0300

    firebase fixing config

--- eshop-MERN(mongo,express,react,node.js)/Frontend/src/Utils/firebase.ts
+++ eshop-MERN(mongo,express,react,node.js)/Frontend/src/Utils/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2zNwERY7gabIy_qSLA3bOKpec7rocF8Y",
-    authDomain: "eshop-607c7.firebaseapp.com",
-    projectId: "eshop-607c7",
-    storageBucket: "eshop-607c7.appspot.com",
-    messagingSenderId: "391844214026",
-    appId: "1:391844214026:web:3f9d7ac0786678e32e76f3",
-    measurementId: "G-2HFHMDEJ1T"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
   };
 
 
